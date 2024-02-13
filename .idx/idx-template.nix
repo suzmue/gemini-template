d94b12f7596c7161e70e117528115e19b55f56d5
Suzy Mueller <suzmue@golang.org>
2024-02-12 16:56:12 -0800

    Rearrange to work as a template

--- .idx/idx-template.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, ... }: {
-  packages = [ pkgs.go ];
-  bootstrap = ''
-    cp -rf ${./.}/${environment} "$WS_NAME"
-    chmod -R +w "$WS_NAME"
-    mv "$WS_NAME" "$out"
-  '';
-}
--- README.md
+++
@@ -1,1 +0,0 @@
-This is a simple demonstration web app using the [Google AI Go SDK](https://github.com/google/generative-ai-go) to generate recipes for images.
