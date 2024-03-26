d6fdffd82519381e403004870157f2aaba87232a
Suzy Mueller <suzmue@golang.org>
2024-03-26 15:27:54 -0700

    Add langchain overlay template

--- idx-template.nix
+++ idx-template.nix
@@ -1,8 +1,10 @@
-{ pkgs, ... }: {
+{ pkgs, useLangChain ? false, ... }: {
   packages = [ pkgs.go ];
   bootstrap = ''
     cp -rf ${./.}/go-gemini "$WS_NAME"
     chmod -R +w "$WS_NAME"
+    ${if useLangChain then "cp -rf ${./.}/langchain-overlay/* $WS_NAME" else "" }
+    chmod -R +wx "$WS_NAME"
     mv "$WS_NAME" "$out"
   '';
 }
