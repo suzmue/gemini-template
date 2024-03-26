4f09231493c92340f6ee5a437837d7d261ce6759
Suzy Mueller <suzmue@golang.org>
2024-03-26 15:40:14 -0700

    try to get template to work

--- idx-template.nix
+++ idx-template.nix
@@ -4,7 +4,6 @@
     cp -rf ${./.}/go-gemini "$WS_NAME"
     chmod -R +w "$WS_NAME"
     ${if useLangChain then "cp -rf ${./.}/langchain-overlay/* $WS_NAME" else "" }
-    chmod -R +wx "$WS_NAME"
     mv "$WS_NAME" "$out"
   '';
 }
